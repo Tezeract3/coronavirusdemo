e14f20d257832d659b74ce94644d55229011d927
THARUKA-D <tharukajn@gmail.com>
2021-07-30 21:43:56 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/coronaliveupdate/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.coronaliveupdate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'screens/homePage.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
