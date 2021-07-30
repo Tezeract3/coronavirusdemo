19f0c2526794a661b6e126a556e6afeff1f14454
chehansivaruban <63904379+chehansivaruban@users.noreply.github.com>
2021-07-30 22:56:00 +0530

    search bar added

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,9 +12,31 @@ class _HomePageState extends State<HomePage> {
     return SafeArea(
       child: Scaffold(
         body: ListView(
-          children: <Widget>[],
+          children: <Widget>[MyPrefilledSearch()],
         ),
       ),
     );
+  }
+}
+
+class MyPrefilledSearch extends StatefulWidget {
+  const MyPrefilledSearch({Key? key}) : super(key: key);
+
+  @override
+  _MyPrefilledSearchState createState() => _MyPrefilledSearchState();
+}
+
+class _MyPrefilledSearchState extends State<MyPrefilledSearch> {
+  late TextEditingController _textController;
+
+  @override
+  void initState() {
+    super.initState();
+    _textController = TextEditingController(text: 'initial text');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoSearchTextField(controller: _textController);
   }
 }
